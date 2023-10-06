4865f370b17cfa9f11ee72b3daeedd4448345db5
Arasi Dobariya <arasidobariya7@gmail.com>
2023-10-06 10:17:47 -0400

    Attaching polyloads  to Action

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -8,7 +8,9 @@ const Counter = () => {
   const incrementHandler = () => {
     dispatch({ type: "increment" });
   };
-
+  const increaseHandler = () => {
+    dispatch({ type: "increase", amount: 5 });
+  };
   const decrementHandler = () => {
     dispatch({ type: "decrement" });
   };
@@ -19,8 +21,9 @@ const Counter = () => {
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
       <div className={classes.value}>{counter}</div>
-      <div >
+      <div>
         <button onClick={incrementHandler}>Increment</button>
+        <button onClick={increaseHandler}>Increment by 5</button>
         <button onClick={decrementHandler}>Decrement</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
